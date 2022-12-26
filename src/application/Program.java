c6be20fed1133b1ddd6873b72b7a0cb1b3c7e174
Edson Santos <cmo.edson@gmail.com>
2022-12-26 08:48:17 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,8 +10,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
 		System.out.println(seller);
 
